e425fbf5969527fbb3cfaf3cf028a7fffed96f5f
ChenYong <chenyong@jimeng.io>
2019-09-10 11:45:20 +0800

    dirty trick for switching working directory

--- src/app/server.cljs
+++ src/app/server.cljs
@@ -10,6 +10,7 @@
             ["chalk" :as chalk]
             ["latest-version" :as latest-version]
             ["url-parse" :as url-parse]
+            ["child_process" :as cp]
             [app.config :as config]
             [cumulo-util.file :refer [write-mildly! get-backup-path! merge-local-edn!]]
             [cumulo-util.core :refer [id! repeat! unix-time! delay!]]
@@ -88,10 +89,29 @@
      (catch js/Error error (js/console.error error)))
     :effect))
 
+(def wd-file-path (path/join js/__dirname "working-directory.text"))
+
+(defn listen-to-switching! []
+  (js/process.on
+   "SIGPIPE"
+   (fn [e]
+     (let [new-path (fs/readFileSync wd-file-path "utf8")]
+       (js/process.chdir new-path)
+       (dispatch! :repo/set-upstream (manager/get-upstream!) "system")
+       (println "Switching to" new-path)))))
+
 (defn on-exit! [code]
   (persist-db!)
   (comment println "exit code is:" (pr-str code))
   (js/process.exit))
+
+(def pid-file-path (path/join js/__dirname "pid.text"))
+
+(defn put-down-pid! []
+  (fs/writeFile
+   pid-file-path
+   js/process.pid
+   (fn [e] (println "Wrote down pid" js/process.pid))))
 
 (defn sync-clients! [reel]
   (wss-each!
@@ -130,7 +150,7 @@
       (dispatch! :session/disconnect nil sid)),
     :on-error (fn [error] (.error js/console error))}))
 
-(defn main! []
+(defn main-server! []
   (println "Running mode:" (if config/dev? "dev" "release"))
   (let [port (js/parseInt (or js/process.env.port (:port config/site)))
         url-obj (url-parse "http://fe.jimu.io/rebase-hell/" true)]
@@ -142,7 +162,17 @@
   (comment js/process.on "SIGINT" on-exit!)
   (comment repeat! 600 #(persist-db!))
   (dispatch! :repo/set-upstream (manager/get-upstream!) "system")
+  (put-down-pid!)
+  (listen-to-switching!)
   (check-version!))
+
+(defn main-switch! []
+  (let [previous-port (fs/readFileSync pid-file-path "utf8")]
+    (fs/writeFileSync wd-file-path (js/process.cwd))
+    (cp/execSync (<< "kill -13 ~{previous-port}"))
+    (println "Message sent" (<< "kill -13 ~{previous-port}"))))
+
+(defn main! [] (if (= (aget js/process.argv 2) "switch") (main-switch!) (main-server!)))
 
 (defn reload! []
   (println "Code updated.")
