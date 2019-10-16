9b3978605873608c16dad21f11644ede2bdac02c
ChenYong <chenyong@jimeng.io>
2019-10-16 14:03:54 +0800

    add support for HTTPS remote url; alpha release

--- src/app/util.cljs
+++ src/app/util.cljs
@@ -1,4 +1,17 @@
 
 (ns app.util (:require [clojure.string :as string]))
 
+(defn grab-upstream [url]
+  (cond
+    (string/starts-with? url "git@")
+      (-> url string/trim (string/split ":") last (string/replace ".git" ""))
+    (string/starts-with? url "https://")
+      (-> url
+          string/trim
+          (string/split ":")
+          last
+          (string/replace "//github.com/" "")
+          (string/replace ".git" ""))
+    :else (throw (js/Error. (str "Invalid url:" url)))))
+
 (defn read-items [x] (-> (or x "") string/trim (string/split "\n")))
